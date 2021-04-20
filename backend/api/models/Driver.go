68dc300c822f11acfc4e03f6c55c822ab955256c
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-04-19 22:29:06 -0300

    created Driver model for the basic structure of the app

--- backend/api/models/Driver.go
+++ backend/api/models/Driver.go
@@ -1,5 +1,10 @@
 package models
 
 type Driver struct {
-	
+	ID        			uint64    `gorm:"primary_key;auto_increment"`
+	Name						string		`gorm:"size:50;not null" json:"name"`
+	Email     			string    `gorm:"size:100;not null;unique" json:"email"`
 }
+
+
+
