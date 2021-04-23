2cc4ed7b6438601decf46014fdd73c79a581f9c6
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-04-23 07:59:05 -0300

    created basic styles and rendering for the travels list

--- frontend/src/components/Travels/Travels.js
+++ frontend/src/components/Travels/Travels.js
@@ -3,14 +3,23 @@ import './Travels.css';
 
 import Travel from  './Travel/Travel';
   
-
   const travels = (props) => {
   
+    //this wil render the list by mapping the array
+    //therefore, the props of Travels component will be an array
+
   return (
-    <div>
-      <ul>
-        <li><Travel driver="Johnny" travelledkilometer="2.5" literspent="1" checkoutdate="25/12/1997" /> </li>
-      </ul>
+    <div className="Travels-Container">  
+      <div className="Descriptions">
+        <p>Driver</p>
+        <p>Travelled Km</p>
+        <p>Liter spent</p>
+        <p>Checkout Date</p>
+      </div>
+      <Travel driver="Johnny" travelledkilometer="2.5" literspent="1" checkoutdate="25/12/1997" />
+      <Travel driver="Johnny" travelledkilometer="2.5" literspent="1" checkoutdate="25/12/1997" />
+      <Travel driver="Johnny" travelledkilometer="2.5" literspent="1" checkoutdate="25/12/1997" />
+      <Travel driver="Johnny" travelledkilometer="2.5" literspent="1" checkoutdate="25/12/1997" />
     </div>
   )
 }
