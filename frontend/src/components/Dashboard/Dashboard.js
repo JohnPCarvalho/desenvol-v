1445470933eb1412e8b6493e19f7db3c25d965b9
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-04-22 21:56:28 -0300

    basic changes to dashboard

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Button, Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
-  return (
-    <h2>Dashboard</h2>
+  return (  
+    <div>
+      <h2>Dashboard</h2>
+      <Link to="/travels"><Button>Travels</Button></Link>
+      <Link to="/preferences"> <Button>Preferences</Button></Link>
+    </div>
   )
 }
