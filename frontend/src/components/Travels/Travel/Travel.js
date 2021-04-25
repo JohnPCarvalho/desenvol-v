f48dc5cbf85527f790f12e368bd769624504bdb8
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-04-25 19:20:13 -0300

    new logic to the Travel component

--- frontend/src/components/Travels/Travel/Travel.js
+++ frontend/src/components/Travels/Travel/Travel.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Travel.css';
 
+import Aux from '../../../hoc/Aux';
+import TravelsControls from '../TravelsControls/TravelsControls'
+
 const travel = (props) => {
   return (
-    <div className="Travel-Container">
-      <p>{props.driver}</p>
-      <p>{props.travelledkilometer} </p>
-      <p>{props.literspent} </p>
-      <p>{props.checkoutdate} </p>
-    </div>
+    <Aux>
+      <div className="Travel-Container">
+        <p>{props.driver}</p>
+        <p>{props.travelledkilometer} </p>
+        <p>R$ {props.priceperliter}</p>
+        <p>{props.literspent} </p>
+        <p>{props.checkoutdate}</p>
+        <TravelsControls plus={props.plus} plus={props.minus} />
+      </div>
+
+    </Aux>
+
   );
 }
 
