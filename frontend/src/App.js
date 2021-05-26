141058e9e7f7de4b2f6f7d139d01bc147f6d6abd
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-05-25 22:17:02 -0300

    refactor App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,14 @@
-import React, { useState } from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import {Router} from 'react-router-dom';
 
-import Dashboard from './components/Dashboard/Dashboard';
-import Login from './components/Login/Login';
-import Preferences from './components/Preferences/Preferences';
-import Travels from './components/Travels/Travels';
-import { AuthProvider } from  '../src/Context/AuthContext';
+import Routes from './routes';
+import history from './history';
 
 function App() {
-  
-  const [token, setToken] = useState()
-
- /*  if(!token) {
-    return <Login setToken={setToken} />
-  } */
-
   return (
-    <AuthProvider>
-      <div className="wrapper">
-      <h1>Desenvol-v</h1>
-      <BrowserRouter>
-          <Switch>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/preferences">
-              <Preferences />
-            </Route>
-            <Route path="/travels">
-              <Travels />
-            </Route>
-          </Switch>
-      </BrowserRouter>
-      </div>
-    </AuthProvider>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
